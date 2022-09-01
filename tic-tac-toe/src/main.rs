9959104ef78d4b45da32bcb75faa38feadca5717
Amon De Shir <amonsir29@gmail.com>
2022-09-01 23:19:13 +0200

    remove greeter

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,22 +1,19 @@
 use bevy::{prelude::{App, ClearColor, Color}, DefaultPlugins, window::WindowDescriptor};
-use greeter::GreeterPlugin;
 use map::MapPlugin;
 
-mod greeter;
 mod map;
 
 fn main() {
   App::new()
     .insert_resource(bevy::render::texture::ImageSettings::default_nearest())  
     .insert_resource(WindowDescriptor {
-      title: "TicTacToe".to_string(),
+      title: "Tic Tac Toe".to_string(),
       width: 512.0,
       height: 512.0,
       ..Default::default()
     })
     .insert_resource(ClearColor(Color::rgb(0.6, 0.6, 0.6)))
     .add_plugins(DefaultPlugins)
-    .add_plugin(GreeterPlugin)
     .add_plugin(MapPlugin)
     .run();
 } 
